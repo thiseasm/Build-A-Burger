1ee30e839aef0705d37bd3832fd1fe0c65edd641
thiseasm <44376778+thiseasm@users.noreply.github.com>
2020-05-24 12:57:31 +0300

    Add Order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,21 @@ class BurgerBuilder extends Component {
 			meat: 0,
 		},
 		totalPrice: 4,
+		purchasable: false,
 	};
+
+	updatePurchaseState(updatedIngredients) {
+		// Calculate the sum of all ingredients
+		const ingredients = { ...updatedIngredients };
+		const sum = Object.keys(ingredients)
+			.map(igKey => {
+				return ingredients[igKey];
+			})
+			.reduce((sum, el) => {
+				return sum + el;
+			}, 0);
+		this.setState({ purchasable: sum > 0 });
+	}
 
 	addIngredientHandler = type => {
 		const oldCount = this.state.ingredients[type];
@@ -36,10 +50,14 @@ class BurgerBuilder extends Component {
 			ingredients: updatedIngredients,
 			totalPrice: newPrice,
 		});
+		this.updatePurchaseState(updatedIngredients);
 	};
 
 	removeIngredientHandler = type => {
 		const oldCount = this.state.ingredients[type];
+		if (oldCount <= 0) {
+			return;
+		}
 		const updatedCount = oldCount - 1;
 		const updatedIngredients = { ...this.state.ingredients };
 		updatedIngredients[type] = updatedCount;
@@ -52,15 +70,26 @@ class BurgerBuilder extends Component {
 			ingredients: updatedIngredients,
 			totalPrice: newPrice,
 		});
+		this.updatePurchaseState(updatedIngredients);
 	};
 
 	render() {
+		const disabledInfo = {
+			...this.state.ingredients,
+		};
+		for (let key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0;
+		}
+
 		return (
 			<Auxiliary>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo}
+					purchasable={this.state.purchasable}
+					price={this.state.totalPrice}
 				/>
 			</Auxiliary>
 		);
