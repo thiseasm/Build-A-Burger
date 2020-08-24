4cb408d4e193e4432f2045643efc21c107f364eb
thiseasm <44376778+thiseasm@users.noreply.github.com>
2020-08-24 11:48:03 +0300

    Change logo height

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,7 +7,9 @@ import classes from './Toolbar.module.css';
 const toolbar = props => (
 	<header className={classes.Toolbar}>
 		<div>MENU</div>
-		<Logo />
+		<div className={classes.Logo}>
+			<Logo />
+		</div>
 		<NavigationItems />
 	</header>
 );
